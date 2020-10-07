8b69b001d63c46bb67ed934f8a1c9b4c6f92b43b
Abhinav Tushar <abhinav@lepisma.xyz>
2020-10-07 22:48:28 +0530

    simple model upgrade

--- ngeo/features.py
+++ ngeo/features.py
@@ -1,5 +1,7 @@
 from typing import List
 
+import scipy as sp
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -10,12 +12,14 @@ class NgeoFeaturizer(BaseEstimator, TransformerMixin):
     """
 
     def __init__(self):
-        self.cvec = CountVectorizer()
+        self.cvec = CountVectorizer(analyzer="char_wb", ngram_range=(1, 3))
+        self.wvec = CountVectorizer()
 
     def fit(self, X: List[str], y=None):
         self.cvec.fit(X)
+        self.wvec.fit(X)
 
         return self
 
     def transform(self, X: List[str], y=None):
-        return self.cvec.transform(X)
+        return sp.sparse.hstack([self.cvec.transform(X), self.wvec.transform(X)])
