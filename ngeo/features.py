36c8e6b8a0a0632bcdc8399de1f56e11abeb2734
Abhinav Tushar <abhinav@lepisma.xyz>
2020-10-09 21:46:49 +0530

    large n-gram ranges

--- ngeo/features.py
+++ ngeo/features.py
@@ -3,7 +3,7 @@ from typing import List
 import scipy as sp
 
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class NgeoFeaturizer(BaseEstimator, TransformerMixin):
@@ -12,14 +12,11 @@ class NgeoFeaturizer(BaseEstimator, TransformerMixin):
     """
 
     def __init__(self):
-        self.cvec = CountVectorizer(analyzer="char_wb", ngram_range=(1, 3))
-        self.wvec = CountVectorizer()
+        self.cvec = TfidfVectorizer(analyzer="char_wb", ngram_range=(1, 10))
 
     def fit(self, X: List[str], y=None):
         self.cvec.fit(X)
-        self.wvec.fit(X)
-
         return self
 
     def transform(self, X: List[str], y=None):
-        return sp.sparse.hstack([self.cvec.transform(X), self.wvec.transform(X)])
+        return sp.sparse.hstack([self.cvec.transform(X)])
